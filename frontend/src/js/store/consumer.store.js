67be9091b9bad2533b5c370a52c820bde2953086
Marcus Coleman <marcus.coleman@bauermedia.co.uk>
2018-12-31 17:03:47 +0000

    Add SET_CONSUMER_LIST

--- frontend/src/js/store/consumer.store.js
+++ frontend/src/js/store/consumer.store.js
@@ -1,6 +1,8 @@
 import fetch from 'js/fetch'
+import * as types from 'store/mutation_types'
 
 const state = {
+	consumers: undefined
 }
 
 const getters = {
@@ -10,6 +12,9 @@ const actions = {
 }
 
 const mutations = {
+	[types.SET_CONSUMER_LIST] (state, consumers) {
+		Vue.set(state, 'user', consumers)
+	}
 }
 
 export default {
