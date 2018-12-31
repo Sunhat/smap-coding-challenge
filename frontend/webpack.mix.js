f1226462aefc3eee9ceed152cfc6150c632ca2da
Marcus Coleman <marcus.coleman@bauermedia.co.uk>
2018-12-31 16:43:03 +0000

    extractVueStyles, globalVueStyles, for components

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -2,3 +2,7 @@ const mix = require('laravel-mix')
 mix
 .js('src/js/app.js', 'app/static/js').extract(['vue', 'vuex', 'axios'])
 .sass('src/scss/app.scss', 'app/static/css')
+.options({
+	extractVueStyles: 'app/static/css/components.css',
+	globalVueStyles: "src/scss/_global.scss"
+})
