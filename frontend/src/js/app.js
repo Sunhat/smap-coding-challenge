72eb43400d8f2fd021156d38f295557b39815640
Marcus Coleman <marcus.coleman@bauermedia.co.uk>
2018-12-31 16:53:24 +0000

    Create VueX Store

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueX from 'vuex'
 import VueRouter from 'vue-router'
 import SuiVue from 'semantic-ui-vue'
 
@@ -7,7 +8,13 @@ import routes from './routes'
 
 // Components
 import Master from 'pages/Master'
+
+// VueX Stores
+import storeModules from './store/store'
+
+
 // Activate in Vue
+Vue.use(VueX)
 Vue.use(VueRouter)
 Vue.use(SuiVue)
 
@@ -16,7 +23,15 @@ const router = new VueRouter({
 	mode:'history',
 	routes,
 })
+
+// Boot the store
+const store = new VueX.Store({
+	modules: storeModules,
+})
+
+// Instantiate Vue
 new Vue({
+	store,
 	router,
 	render: h => h(Master),
 }).$mount('#vue')
