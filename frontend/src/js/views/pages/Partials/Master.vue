0f381617d5bf1c6d94592a1079a62b25a6f1eaea
Marcus Coleman <Sunhat@users.noreply.github.com>
2019-01-02 18:07:10 +0000

    Implement alert system to site

--- frontend/src/js/views/pages/Partials/Master.vue
+++ frontend/src/js/views/pages/Partials/Master.vue
@@ -1,16 +1,19 @@
 <template>
 	<div>
 		<loading-screen />
+		<alert/>
 		<heading />
 			<router-view></router-view>
 	</div>
 </template>
 import Heading from 'pages/Partials/Heading'
 import LoadingScreen from 'components/LoadingScreen'
+import Alert from 'components/Alert'
 export default {
 	components: {
 		Heading,
 		LoadingScreen,
+		Alert
 	},
 }
 </script>
