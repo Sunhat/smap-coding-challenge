022a8dacae0fba4d97298adbd7403089236cfcde
Marcus Coleman <Sunhat@users.noreply.github.com>
2019-01-02 18:06:37 +0000

    Add heading

---
+++ frontend/src/js/views/pages/Partials/Heading.vue
@@ -0,0 +1,37 @@
+<template>
+	<header>
+		<sui-menu pointing secondary class="huge">
+			<router-link
+				tag="sui-menu-item"
+				active-class="active"
+				v-for="(item, index) in items"
+				:key="index"
+				:to="item.to">
+				{{ item.text }}
+			</router-link>
+		</sui-menu>
+	</header>
+</template>
+
+
+<script>
+export default {
+	data () {
+		return {
+			items: [
+				{
+					to: '/',
+					text: 'Consumers'
+				}
+			]
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+	background-color: #fff;
+	margin-bottom: 1em;
+}
+</style>
--- frontend/src/js/views/pages/Partials/Master.vue
+++ frontend/src/js/views/pages/Partials/Master.vue
@@ -1,12 +1,15 @@
 <template>
 	<div>
 		<loading-screen />
+		<heading />
 			<router-view></router-view>
 	</div>
 </template>
+import Heading from 'pages/Partials/Heading'
 import LoadingScreen from 'components/LoadingScreen'
 export default {
 	components: {
+		Heading,
 		LoadingScreen,
 	},
 }
